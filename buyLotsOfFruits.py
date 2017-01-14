16fce0629b1a26ca90dfa379e0e57efdce70e7ec
Chris Gradwohl <christophergradwohl@gmail.com>
2017-01-14 09:48:57 -0800

    cleaned up

--- buyLotsOfFruits.py
+++ buyLotsOfFruits.py
@@ -43,5 +43,3 @@ if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
     orderList = [ ('apples', 2.0), ('pears', 3.0), ('limes', 4.0) ]
     print 'Cost of', orderList, 'is', buyLotsOfFruit(orderList)
-
-    
